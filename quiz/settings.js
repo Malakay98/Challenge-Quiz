528fadb9393c32239812c1119381d924796b6827
Frank <franfd_98@hotmail.com>
2022-01-14 17:06:41 -0300

    All changes saved

---
+++ quiz/settings.js
@@ -0,0 +1,59 @@
+import Quiz from './quiz.js';
+
+class Settings {
+  constructor() {
+    this.quizElement = document.querySelector('.quiz');
+    this.settingsElement = document.querySelector('.settings');
+    this.category = document.querySelector('#category');
+    this.numberOfQuestions = document.querySelector('#questions');
+    this.difficulty = [
+      document.querySelector('#easy'),
+      document.querySelector('#medium'),
+      document.querySelector('#hard'),
+    ];
+    this.startButton = document.querySelector('#start');
+
+    this.quiz = { };
+
+    this.startButton.addEventListener('click', this.startQuiz.bind(this));
+  }
+
+  async startQuiz() {
+    try {
+      const categoryId = this.category.value;
+      const difficulty = this.getCurrentDifficulty();
+
+      const url = `https://opentdb.com/api.php?amount=5&category=${categoryId}&difficulty=${difficulty}&type=multiple`;
+
+      let data = await this.fetchData(url);
+      this.toggleVisibility();
+      this.quiz = new Quiz(this.quizElement, data.results);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  toggleVisibility() {
+    this.settingsElement.style.visibility = 'hidden';
+    this.quizElement.style.visibility = 'visible';
+  }
+
+  async fetchData(url) {
+    const response = await fetch(url);
+    const result = await response.json();
+
+    return result;
+  }
+
+  getCurrentDifficulty() {
+    const checkedDifficulty = this.difficulty.filter(element => element.checked);
+
+    if (checkedDifficulty.length === 1) {
+      return checkedDifficulty[0].id;
+    } else {
+      throw new Error('Please select a difficulty!');
+    }
+  }
+}
+
+export default Settings;
